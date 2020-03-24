2cba6f3ba05b6c016fdb8f4c04ee15adcb3f54ff
1Mill <erik.ekberg@outlook.com>
2020-03-23 19:38:06 -0700

    Scaffold example functions

---
+++ main.rb
@@ -0,0 +1,14 @@
+f = -> x { x + 2 }
+g = -> x { x * 2 }
+
+# puts [1,2,3].map(&f).map(&g)
+
+my_function = -> x { x + 2 } >> -> x { x * 2 }
+my_other_function = f >> g >> -> x { puts x }
+
+puts my_function.call(2)
+puts my_function.call(3)
+puts my_function.call(4)
+puts my_function.call(5)
+
+my_other_function.call(100)
